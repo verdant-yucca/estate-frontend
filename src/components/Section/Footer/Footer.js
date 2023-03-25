a4e6ab0451211c063080bc94ae6e5dc57cda23d4
Roman Davydenko <Deadshot323rd@yandex.ru>
2023-03-26 01:17:37 +0800

    начал делать футер, страницу Услуги и О компании

--- src/components/Section/Footer/Footer.js
+++ src/components/Section/Footer/Footer.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Footer.css';
+import {Link, NavLink} from "react-router-dom";
+import logo from "../../../images/logo/logo_white.png";
 
 function Footer() {
   const year = new Date().getFullYear();
   return (
     <footer className="footer">
+      <div className='footer__delimiter'></div>
+      <div className='footer__columns'>
+        <div className='footer__column'>
+          <Link to="/">
+            <img className={'footer__logo'} src={logo} alt="логотип" />
+          </Link>
+          <p className='footer__logo-text'>Lorem ipsum dolor sit amet, consec tetur adipisicing elit, sed do eiusmod tempor incididuntut consec tetur adipisicing elit,Lorem ipsum dolor sit amet.</p>
+        </div>
+        <div className='footer__column'>
+          <div className={'footer__nav'} >
+            <h2 className='footer__nav-title'>Меню</h2>
+            <NavLink activeClassName="active-link" className={'footer__button'} exact to="/">Главная</NavLink>
+            <NavLink activeClassName="active-link" className={'footer__button'} to="/estate">Недвижимость</NavLink>
+            <NavLink activeClassName="active-link" className={`footer__button`} to="/services">Услуги</NavLink>
+            <NavLink activeClassName="active-link" className={`footer__button`} to="/about">О компании</NavLink>
+          </div>
+        </div>
+        <div className='footer__column'></div>
+      </div>
       <p className="footer__copyright">&copy; {year} Агентство недвижимости</p>
+
     </footer>
   );
 }
