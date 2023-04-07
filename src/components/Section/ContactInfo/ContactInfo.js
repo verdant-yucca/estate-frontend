7d2ade3247b6a262aeba572210ee56217e56b45c
Roman Davydenko <deadshot323rd@yandex.ru>
2023-04-08 00:53:30 +0800

    доделали контактную информацию

--- src/components/Section/ContactInfo/ContactInfo.js
+++ src/components/Section/ContactInfo/ContactInfo.js
@@ -1,13 +1,13 @@
 import './ContactInfo.css';
 import ContactUsForm from "../Blocks/ContactUsForm/ContactUsForm";
 import {
+  EnvironmentOutlined,
   FacebookOutlined,
-  GooglePlusOutlined,
-  MailOutlined, PhoneFilled, PhoneOutlined,
-  PhoneTwoTone,
-  RedditOutlined,
-  TwitterOutlined
+  GooglePlusOutlined, LinkedinOutlined,
+  MailOutlined, PhoneFilled,
+  TwitterOutlined, WechatOutlined
 } from "@ant-design/icons";
+import React from "react";
 
 // function EstateDescription({title,price,images}) {
 function ContactInfo() {
@@ -15,20 +15,27 @@ function ContactInfo() {
     <section className="contact-info">
       <h2 className="contact-info__title">Контакты</h2>
       <div className={"contact-info__block"}>
-        <ContactUsForm/>
         <div  className={"contact-info__column"}>
+          <ContactUsForm/>
+        </div>
 
+        <div  className={"contact-info__column"}>
+          <h3 className={"contact-info__subtitle"}>Контактная информация</h3>
+          <p className={"contact-info__address margin-bottom"}>В данном разделе вы можете ознакомиться с контактной информацией</p>
 
+          <h3 className={"contact-info__phone"}><PhoneFilled  className={"contact-info__image"}/> +7 (950) 451 23 67</h3>
+          <p className={"contact-info__address"}><EnvironmentOutlined/>  &nbsp; г. Ангарск, Карла Маркса 170, этаж 3 </p>
+          <p className={"contact-info__address margin-bottom"}><MailOutlined/> &nbsp; contact@superestate.ru</p>
 
-          <MailOutlined  className={"contact-info__image"}/>
+          <h3 className={"contact-info__subtitle"}>Будьте с нами в соцсетях</h3>
+          <div>
+            <TwitterOutlined className={"contact-info__social-image"} />
+            <WechatOutlined className="contact-info__social-image" />
+            <LinkedinOutlined  className={"contact-info__social-image"}/>
+            <FacebookOutlined  className={"contact-info__social-image"}/>
+            <GooglePlusOutlined  className={"contact-info__social-image"}/>
+          </div>
 
-           <p className={"contact-info__phone"}><PhoneFilled  className={"contact-info__image"}/> +7 (950) 000 0000</p>
-
-
-          <FacebookOutlined  className={"contact-info__image"}/>
-          <TwitterOutlined className={"contact-info__image"} />
-          <GooglePlusOutlined  className={"contact-info__image"}/>
-          <RedditOutlined  className={"contact-info__image"}/>
 
         </div>
       </div>
