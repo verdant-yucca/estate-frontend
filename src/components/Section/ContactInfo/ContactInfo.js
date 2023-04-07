32ba8c49cc1e654ca2cd3cced2549ef5604b1ae4
Roman Davydenko <deadshot323rd@yandex.ru>
2023-04-07 23:41:22 +0800

    продолжили делать контактную информацию

--- src/components/Section/ContactInfo/ContactInfo.js
+++ src/components/Section/ContactInfo/ContactInfo.js
@@ -1,15 +1,38 @@
 import './ContactInfo.css';
 import ContactUsForm from "../Blocks/ContactUsForm/ContactUsForm";
+import {
+  FacebookOutlined,
+  GooglePlusOutlined,
+  MailOutlined, PhoneFilled, PhoneOutlined,
+  PhoneTwoTone,
+  RedditOutlined,
+  TwitterOutlined
+} from "@ant-design/icons";
 
 // function EstateDescription({title,price,images}) {
 function ContactInfo() {
   return (
     <section className="contact-info">
       <h2 className="contact-info__title">Контакты</h2>
-      <ContactUsForm/>
-      <div className={"contact-info__column"}>
-        Test text Test text Test text Test text Test text Test text
+      <div className={"contact-info__block"}>
+        <ContactUsForm/>
+        <div  className={"contact-info__column"}>
+
+
+
+          <MailOutlined  className={"contact-info__image"}/>
+
+           <p className={"contact-info__phone"}><PhoneFilled  className={"contact-info__image"}/> +7 (950) 000 0000</p>
+
+
+          <FacebookOutlined  className={"contact-info__image"}/>
+          <TwitterOutlined className={"contact-info__image"} />
+          <GooglePlusOutlined  className={"contact-info__image"}/>
+          <RedditOutlined  className={"contact-info__image"}/>
+
+        </div>
       </div>
+
 
     </section>
   )
